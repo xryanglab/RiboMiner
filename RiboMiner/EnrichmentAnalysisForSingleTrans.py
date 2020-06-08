e71984e2be90d3730a17f8dc767f2ce721a23c2c
Sherking <sherkinglee@gmai.com>
2020-06-08 11:53:18 +0800

    update

--- RiboMiner/EnrichmentAnalysisForSingleTrans.py
+++ RiboMiner/EnrichmentAnalysisForSingleTrans.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:UTF-8 -*-
 '''
-@Author: Li Fajin
-@Date: 2019-08-29 09:25:05
-@LastEditors: Li Fajin
-@LastEditTime: 2020-06-08 11:26:47
-@Description: This script is used for plot of enrichment ratio for a single transcript
 usage: python EnrichmentAnalysisForSingleTrans.py -i <all_ratio.txt> -o <output_prefix> -c <coorFile> -s transcript_id --id-type transcript_id --unit codon [-S| --ymin|--ymax...]
 '''
 
